a122a5362c041a4d70fa3d26c55d8670f25dfe11
LuisMatosDev <kagenoryuu@gmail.com>
2025-12-04 17:11:27 +0000

    Interfaces
    Criação e implementação de interfaces simples.

---
+++ src/main/java/oop/tp6/POO_TP6.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package oop.tp6;
+
+/**
+ *
+ * @author Luis Matos
+ */
+public class POO_TP6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
